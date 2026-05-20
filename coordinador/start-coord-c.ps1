0161f4fc9b81b2efffef0461de95293ed2cb5f30
maicolchavarro <mschavarro@uniboyaca.edu.co>
2026-05-20 14:24:44 -0500

    link otro cordinadoer

--- coordinador/start-coord-c.ps1
+++ coordinador/start-coord-c.ps1
@@ -29,8 +29,8 @@ $env:PORT = "5002"
 $env:PEER_PORT = "5102"
 $env:COORDINATOR_ID = "coord-c"
 
-# Reemplaza esta URL por el tercer ngrok antes de la demo publica.
-$env:PUBLIC_WS_URL = "ws://localhost:5002"
+# url nuevo cordinador
+$env:PUBLIC_WS_URL = "wss://breadless-keren-topfull.ngrok-free.dev"
 $env:PEER_WS_URL = "ws://localhost:5102"
 
 Set-Location $PSScriptRoot
